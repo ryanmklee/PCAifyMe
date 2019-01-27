34cee5e55b5672762c0492aa18b86ac391a03ff9
graceluong <31752995+graceluong@users.noreply.github.com>
2019-01-26 17:20:22 -0800

    Revert "Merge branch 'justin'"
    This reverts commit ef0348fd8ff52adead54160f470c9839a440f50f, reversing
    changes made to 39bc1b318947a04f3c5d263ef32c65ad3d9d21d3.

--- group-unlock/src/App.vue
+++ group-unlock/src/App.vue
@@ -1,77 +1,28 @@
 <template>
-    <div id="app">
-        <div><video ref="video" id="video" width="640" height="480" autoplay></video></div>
-        <div><button id="snap" v-on:click="capture()">Snap Photo</button></div>
-        <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
-        <ul>
-            <li v-for="c in captures">
-                <img v-bind:src="c" height="50" />
-            </li>
-        </ul>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Group Face Unlock"/>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'app',
-        data() {
-            return {
-                video: {},
-                canvas: {},
-                captures: []
-            }
-        },
-        mounted() {
-    this.video = this.$refs.video;
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-            this.video.srcObject = stream;
-            this.video.play();
-        });
-    }
-},
-        methods: {
-    capture() {
-        this.canvas = this.$refs.canvas;
-        var context = this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480);
-        this.captures.push(canvas.toDataURL("image/png"));
-        var formData = new FormData();
-        formData.append(canvas.toDataURL("image/png"))
+import HelloWorld from './components/HelloWorld.vue'
 
-        $.ajax({
-          url: 'http://127.0.0.1:5000/api/add',
-          type: POST,
-          data: formData,
-          async: false,
-          cache: false,
-          contentType: false,
-          processData: false,
-          success: function(status){
-            console.log("Works!")
-          }
-        })
-    }
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
-    }
 </script>
 
 <style>
-    body: {
-        background-color: #F0F0F0;
-    }
-    #app {
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-    #video {
-        background-color: #000000;
-    }
-    #canvas {
-        display: none;
-    }
-    li {
-        display: inline;
-        padding: 5px;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- group-unlock/src/components/HelloWorld.vue
+++ group-unlock/src/components/HelloWorld.vue
@@ -1,113 +1,91 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>Hi</h1>
+    <div><video ref="video" id="video" width="640" height="480" autoplay></video></div>
+        <div><button id="snap" v-on:click="capture()">Snap Photo</button></div>
+        <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
+        <ul>
+            <li v-for="c in captures">
+                <img v-bind:src="c" height="50" />
+            </li>
+        </ul></div>
+
+    <!-- <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h3>Essential Links</h3>
     <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      video: {},
+      canvavs: {},
+      captures: []
+    }
+  },
+  mounted() {
+    this.video = this.$refs.video;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+            this.video.src = window.URL.createObjectURL(stream);
+            this.video.play();
+        });
+    }
+  },
+  methods: {
+    capture() {
+        this.canvas = this.$refs.canvas;
+        var context = this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480);
+        this.captures.push(canvas.toDataURL("image/png"));
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+    body: {
+        background-color: #F0F0F0;
+    }
+    .hello {
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    #video {
+        background-color: #000000;
+    }
+    #canvas {
+        display: none;
+    }
+    li {
+        display: inline;
+        padding: 5px;
+    }
 </style>
